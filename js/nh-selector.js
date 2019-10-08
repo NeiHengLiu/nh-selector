8d180a72be329548f51b69a9973e28ded67c9650
lnh <scjlnh@163.com>
2019-10-08 16:10:48 +0800

    添加注释

--- js/nh-selector.js
+++ js/nh-selector.js
@@ -29,21 +29,25 @@
       // 初始化所需的位置数据
       self.initLocationData();
 
+      // 列表框位置
+      self.setDomPosition();
+
       // 设置下拉选择列表位置
       self.bind(self.$element, 'click', function(){
         self.dataProcessing(self.options.data)
 
-        self.setDomPosition();
+        // self.setDomPosition();
 
         self.show();
 
         // 临时用一下
         console.log(self.options.data);
-        self.$body.append(self.dataProcessing(self.options.data))
+        //self.$body.append(self.dataProcessing(self.options.data));
       })
 
       return self.$element;
     },
+    // 初始化位置信息
     initLocationData: function(){
       this.locationData = {
         window: {
@@ -53,6 +57,7 @@
         dom: this.$element[0].getBoundingClientRect()
       }
     },
+    // 渲染下拉选择列表
     dataProcessing: function(data){
       var self = this;
       var $selectorMenu = $('<ul class="nh-selector-menu"></ul>');
@@ -67,12 +72,13 @@
                 index = $this.index(),
                 parentIndex = $this.parent().index();
 
+            $this.parent().nextAll().remove();
+
             if(data[index].children !== undefined){ //  还有下一级
               self.callData[parentIndex] = self.dataScreening(data[index]);
               self.dataProcessing(data[index].children);
-              self.options.callback(self.callData); // 临时用一下
             } else {  // 没有下一级
-              
+              self.callData[parentIndex] = self.dataScreening(data[index]);
               self.options.callback(self.callData);
             }
           });
@@ -81,6 +87,7 @@
       }
       return $selectorMenu;
     },
+    // 克隆数据
     dataScreening: function(data){
       var dataObject = {};
       for(var k in data){
@@ -90,6 +97,7 @@
       }
       return dataObject;
     },
+    // 设置下拉列表定位
     setDomPosition: function(){
       this.$selector.css({
         position: 'absolute',
@@ -97,12 +105,15 @@
         left: this.locationData.dom.left + 'px'
       });
     },
+    // 事件绑定
     bind: function(ele, event, fn){
       $(ele).off().on(event, fn);
     },
+    // 事件解绑
     unbind: function(ele){
       $(ele).off();
     },
+    // 显示下拉列表
     show: function(){
       if($('.nh-selector').length > 0){
         this.$selector.show();
